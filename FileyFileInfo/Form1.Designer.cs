3e6275e2c25982c586db82d6abedd88e857cb26e
inicio <inicio@FISEP09>
2024-05-06 09:00:03 -0600

    Agregar archivos de proyecto.

---
+++ FileyFileInfo/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace FileyFileInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(78, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Crear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 65);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(390, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 65);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "escribir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(546, 66);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 65);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Leer";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(170, 208);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(442, 176);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ FileyFileInfo/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileyFileInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("Archivo2.txt"))
+            {
+                //File.Create("Archivo1.txt");
+                FileInfo archivo = new FileInfo("Archivo2.txt");
+                archivo.Create();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            File.Delete("Archivo2.txt");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("Archivo3.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine("esta es la linea uno");
+            sw.WriteLine("linea 2 prueba");
+            sw.Flush(); //para mandar lo escrito
+            sw.Close();
+            fs.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("Archivo3.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string texto = "";
+            //ciclo while nos permite leer hasta cierta condicion "hasta que no existan caracteres"
+            while(!sr.EndOfStream) 
+            {
+                texto += sr.ReadLine() + Environment.NewLine;
+            }
+            sr.Close();
+            fs.Close(); 
+
+            textBox1.Text = texto;
+        }
+    }
+}
